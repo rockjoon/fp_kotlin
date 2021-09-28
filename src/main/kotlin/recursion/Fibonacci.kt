43aabec548794a210566f819181d3b27c30deaa5
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-09-28 23:27:00 +0900

    feat : fibonacci memoization

---
+++ src/main/kotlin/recursion/Fibonacci.kt
@@ -0,0 +1,24 @@
+package recursion
+
+fun main() {
+    println(fiboRecursive(7))
+    println(fiboMemoization(7))
+}
+
+fun fiboRecursive(n: Int): Int {
+    return when(n) {
+        0 -> 0
+        1 -> 1
+        else -> fiboRecursive(n-2) + fiboRecursive(n-1)
+    }
+}
+
+var memoFibonacci = Array(100) { -1 }
+fun fiboMemoization(n: Int): Int {
+    return when {
+        n == 0 -> 0
+        n == 1 -> 1
+        memoFibonacci[n] != -1 -> memoFibonacci[n]
+        else -> fiboMemoization(n - 2) + fiboMemoization(n - 1)
+    }
+}
